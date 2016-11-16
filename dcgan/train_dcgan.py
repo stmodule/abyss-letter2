7cd3b2cbb6e646538fd079fbd18a5ef0045c6f52
araki <araki@qoncept.co.jp>
2016-11-17 08:02:24 +0900

    save good setting

--- dcgan/train_dcgan.py
+++ dcgan/train_dcgan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Flatten, Activation, Reshape
+from keras.layers import Dense, Flatten, Activation
 from keras.layers.convolutional import Convolution2D, UpSampling2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.normalization import BatchNormalization
@@ -10,7 +10,7 @@ from keras.optimizers import Adam, SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping, Callback
 import numpy as np
-import os, math, sys
+import os, math, sys, time
 
 files_dir = os.path.dirname(__file__)
 npy_path = os.path.join(files_dir, "./dcgan_train_data.npy")
@@ -25,28 +25,24 @@ def create_random_features(num):
                             size=[num, 4, 4, 8])
 
 # define models
-
-# 32x32 [-1,1] image
 discriminator = Sequential([
-    Convolution2D(32, 5, 5, border_mode='same', subsample=(2, 2), input_shape=[32, 32, 1]),
-    LeakyReLU(), # [16, 16, 32]
-    Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2)),
-    LeakyReLU(), # [8, 8, 64]
-    Convolution2D(128, 3, 3, border_mode='same', subsample=(2, 2)),
-    LeakyReLU(), # [4, 4, 64]
+    Convolution2D(64, 3, 3, border_mode='same', subsample=(2,2), input_shape=[32, 32, 1]),
+    LeakyReLU(),
+    Convolution2D(128, 3, 3, border_mode='same', subsample=(2,2)),
+    LeakyReLU(),
+    Convolution2D(256, 3, 3, border_mode='same', subsample=(2,2)),
+    LeakyReLU(),
     Flatten(),
-    Dense(1024),
+    Dense(2048),
     LeakyReLU(),
     Dense(1, activation='sigmoid')
 ], name="discriminator")
 
 # input shape=[4, 4, 8] range=[-1, 1]
 generator = Sequential([
-    Convolution2D(32, 3, 3, border_mode='same', input_shape=[4, 4, 8]),
-    BatchNormalization(),
-    ELU(),
+    Convolution2D(64, 3, 3, border_mode='same', input_shape=[4, 4, 8]),
     UpSampling2D(), # 8x8
-    Convolution2D(64, 3, 3, border_mode='same'),
+    Convolution2D(128, 3, 3, border_mode='same'),
     BatchNormalization(),
     ELU(),
     UpSampling2D(), #16x16
@@ -60,8 +56,7 @@ generator = Sequential([
 # setup models
 
 print("setup discriminator")
-set_trainable(discriminator, True)
-opt_d = Adam(lr=0.000005, beta_1=0.1)
+opt_d = Adam(lr=1e-5, beta_1=0.1)
 discriminator.compile(optimizer=opt_d, 
                       loss='binary_crossentropy', 
                       metrics=['accuracy'])
@@ -69,12 +64,10 @@ discriminator.compile(optimizer=opt_d,
 print("setup dcgan")
 set_trainable(discriminator, False)
 dcgan = Sequential([generator, discriminator])
-opt_g = Adam(lr=0.0002, beta_1=0.5)
+opt_g = Adam(lr=2e-4, beta_1=0.5)
 dcgan.compile(optimizer=opt_g, 
               loss='binary_crossentropy', 
               metrics=['accuracy'])
-
-
 
 
 # training
@@ -87,9 +80,13 @@ def evaluate(generate_batch_num = 1000):
     faked = np.sum(pred>0.5)
     return faked
 
-batch_size = 100
+batch_size = 200
 wait = 0
-faked_curve = np.array([])
+test_num = 1000
+rnd_test = create_random_features(test_num)
+faked_curve = np.zeros([0, 2])
+met_curve = np.zeros([0, 4])
+start = time.time()
 for epoch in range(1, sys.maxsize):
 
     print("epoch: {0}".format(epoch))
@@ -97,47 +94,34 @@ for epoch in range(1, sys.maxsize):
     np.random.shuffle(X_train)
     rnd = create_random_features(len(X_train))
 
-    val_interval = 50
-    rnd_val = create_random_features(1000)
-    y_rnd_val = [0]*len(rnd_val)
-
     # train on batch
     for i in range(math.ceil(len(X_train)/batch_size)):
         print("batch:", i, end='\r')
         X_batch = X_train[i*batch_size:(i+1)*batch_size]
         rnd_batch = rnd[i*batch_size:(i+1)*batch_size]
 
+        loss_g, acc_g = dcgan.train_on_batch(rnd_batch, [0]*len(rnd_batch))
         generated = generator.predict(rnd_batch)
-        y_batch = [0]*len(X_batch) + [1]*len(generated)
-        X_batch = np.append(X_batch, generated, axis=0)
-        _, ad = discriminator.train_on_batch(X_batch, y_batch)
+        X = np.append(X_batch, generated, axis=0)
+        y = [0]*len(X_batch) + [1]*len(generated)
+        loss_d, acc_d = discriminator.train_on_batch(X,y)
+        
 
-        if i%val_interval == 0:
-            print("")
-            generated = generator.predict(rnd_val)
-            loss_d, acc_d = discriminator.evaluate(generated, [1]*len(generated))
-
-        _, ag = dcgan.train_on_batch(rnd_batch, [0]*len(rnd_batch))
-
-        if i%val_interval == 0:
-            loss_g, acc_g = dcgan.evaluate(rnd_val, y_rnd_val)
-            print("")
-            print("dis: loss: {0:.5f} acc: {1:.3f}".format(loss_d, acc_d))
-            print("gen: loss: {0:.5f} acc: {1:.3f}".format(loss_g, acc_g))
+        met_curve = np.append(met_curve, [[loss_d, acc_d, loss_g, acc_g]], axis=0)
     
     # output
-    test_num = 1000
-    rnd_test = create_random_features(test_num)
-    _, faked = dcgan.evaluate(rnd_test, [0]*test_num)
-    print("")
-    print("epoch end:", ad, ag)
+    val_loss, faked = dcgan.evaluate(rnd_test, [0]*test_num)
+    print("epoch end:")
+    print("d: loss: {0:.3e} acc: {1:.3f}".format(loss_d, acc_d))
+    print("g: loss: {0:.3e} acc: {1:.3f}".format(loss_g, acc_g))
     print("faked: {0}".format(faked))
 
-    np.append(faked_curve, faked)
+    faked_curve = np.append(faked_curve, [[val_loss, faked]])
     np.save(os.path.join(files_dir, "./faked_curve"), faked_curve)
+    np.save(os.path.join(files_dir, "./met_curve"), met_curve)
 
     # save model
-    if epoch%50 == 0:
+    if epoch%10 == 0:
         # avoiding bug
         # https://github.com/fchollet/keras/pull/4338
         model = Sequential([generator, discriminator])
@@ -145,10 +129,11 @@ for epoch in range(1, sys.maxsize):
         print("Save: {0}.h5".format(epoch))
     print("")
 
-    if acc_d==0 or faked==0:
+    if faked==0 or faked==1:
         wait += 1
-        if wait>100:
-            print("wait reach 100")
+        if wait>50:
+            print("wait reach 50")
+            print("elapsed time: {0}sec".format(time.time()-start))
             exit(0)
     else:
         wait = 0
